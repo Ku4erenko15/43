e373b5c4b59e36504c0e5e08986017620c84f69c
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-05-22 11:08:52 +0300

    Add files via upload

---
+++ 43.2/Notebook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43._2
+{
+    struct Notebook
+    {
+        private string model;
+        private string manuf;
+        private double price;
+
+        public string Model { get => model; set => model = value; }
+        public string Manuf { get => manuf; set => manuf = value; }
+        public double Price { get => price; set => price = value; }
+
+        public Notebook(string model, string manuf, double price) : this()
+        {
+            Model = model;
+            Manuf = manuf;
+            Price = price;
+        }
+
+        public override string ToString() => $"Модель:{Model}" +
+            $" Производитель:{Manuf}" +
+            $" Цена:{Price}$";
+    }
+}
---
+++ 43.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook();
+            notebook.Manuf = "TТИип";
+            notebook.Model = "Учебный ПК";
+            notebook.Price = 200;
+            Console.WriteLine(notebook.ToString());
+            Console.ReadKey();
+        }
+    }
+}
