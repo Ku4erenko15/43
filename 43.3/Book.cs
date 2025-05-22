1b119450cbc5ee908c7946f2aa7e3427c5b30b2b
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-05-22 11:09:13 +0300

    Add files via upload

---
+++ 43.3/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43._3
+{
+    struct Book
+    {
+        public string Title;
+        public string Author;
+        public int Year;
+        public int Pages;
+
+        public Book(string title, string author, int year, int pages)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            Pages = pages;
+        }
+
+        public override string ToString()
+        {
+            return $"Название: {Title}\nАвтор: {Author}\nГод издания: {Year}\nСтраниц: {Pages}\n";
+        }
+    }
+}
---
+++ 43.3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество книг: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Book[] books = new Book[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nКнига {i + 1}:");
+
+                Console.Write("Название: ");
+                string title = Console.ReadLine();
+
+                Console.Write("Ф.И.О. автора: ");
+                string author = Console.ReadLine();
+
+                Console.Write("Год издания: ");
+                int year = int.Parse(Console.ReadLine());
+
+                Console.Write("Количество страниц: ");
+                int pages = int.Parse(Console.ReadLine());
+
+                books[i] = new Book(title, author, year, pages);
+            }
+
+            Console.WriteLine("\n--- Информация о всех книгах ---");
+            for (int i = 0; i < books.Length; i++)
+            {
+                Console.WriteLine($"Книга {i + 1}:\n{books[i]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
